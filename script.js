9fcdb13b8bbc9076d6d678841a2c56a1413bd18b
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-01-31 17:44:50 +0200

    Add posibility to toggle borders for squares
    - Create toggleSquareBorders() function, which turns on and off borders
      for squares
    - Create squareBerdersUsed() function, which checks if borders are used
    - Edit updateGrid() function, so it'll remember if borders were turned
      on

--- script.js
+++ script.js
@@ -5,20 +5,50 @@ function updateGrid(gridSize){
 
   // find number and size of new squares
   const squareNumber = gridSize ** 2;
-  const squareSize = Math.sqrt((grid.clientHeight * grid.clientWidth) / squareNumber)
+  const squareSize = Math.sqrt((grid.clientHeight * grid.clientWidth) / squareNumber);
 
   // apply styles/classes to new squares
   square.style.width = `${squareSize}px`;
   square.style.height = `${squareSize}px`;
-  square.classList.add("square")
+  square.classList.add("square");
 
   // remove old squares from grid
+  let borders = squareBordersUsed(); // remember if borders were active
   while(grid.firstChild){
-    grid.removeChild(grid.firstChild)
+    grid.removeChild(grid.firstChild);
   }
 
   // add new squares to grid
   for(let i = 0; i < squareNumber; i++){
     grid.append(square.cloneNode());
+  }
+
+  // add borders to new squares if they were used with old squares
+  if(borders == true) toggleSquareBorders();
+}
+
+// check if borders are on or off
+function squareBordersUsed(){
+  const grid = document.querySelector(".grid");
+
+  // return false if grid is empty
+  if(!grid.firstElementChild) return false;
+
+  // return true of false, depending on square borders
+  return grid.firstChild.style.border == "thin solid black" ? true : false;
+}
+
+// turn borders on or off
+function toggleSquareBorders(){
+  const grid = document.querySelector(".grid");
+
+  if(squareBordersUsed()){
+    for(square of grid.children){
+      square.style.border = "none";
+    }
+  } else {
+    for(square of grid.children){
+      square.style.border = "thin solid black";
+    }
   }
 }
